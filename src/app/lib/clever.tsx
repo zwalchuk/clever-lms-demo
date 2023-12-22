58780b3a7bec855bfa980ee2cc96a8a5dcdda577
Ashley Gau <ashley.gau@clever.com>
2023-12-21 16:47:58 -0800

    implement clever data fetching

---
+++ src/app/lib/clever.tsx
@@ -0,0 +1,66 @@
+import { Student, Section } from '@/app/lib/definitions';
+
+export class CleverDataFetcher {
+  token: string;
+
+  constructor(token) {
+    this.token = token;
+  }
+
+  async fetch(url: string) {
+    const res = await fetch(url, {
+      headers: {
+          'Authorization': 'Bearer ' + this.token,
+          'Content-Type': 'application/json'
+      },
+    });
+    
+    const data = await res.json();
+
+    if (res.status !== 200) {
+      throw new Error(data.message);
+    }
+    return data;
+  }
+  async fetchStudents() {
+    const students = await this.fetch('https://api.clever.com/v3.0/users?role=student');
+    return students.data.map((data) => new Student(data.data));
+  }
+
+  async fetchSections() {
+    const section = await this.fetch('https://api.clever.com/v3.0/sections');
+    return section.data.map((data) => new Section(data.data));
+  }
+
+  async createAssignment() {
+    // TODO: Implement this
+  }
+
+  async getAssignment() {
+    // TODO: Implement this
+  }
+
+  async updateAssignment() {
+    // TODO: Implement this
+  }
+
+  async deleteAssignment() {
+    // TODO: Implement this
+  }
+
+  async createSubmission() {
+    // TODO: Implement this
+  }
+
+  async getSubmission() {
+    // TODO: Implement this
+  }
+
+  async updateSubmission() {
+    // TODO: Implement this
+  }
+
+  async deleteSubmission() {
+    // TODO: Implement this
+  }
+}
---
+++ src/app/lib/definitions.tsx
@@ -0,0 +1,69 @@
+// This file contains type definitions for Clever-related data.
+export class Student {
+  id: string;
+  district: string;
+  email: string;
+  name: Name;
+  dob: string;
+  gender: string;
+  grade: string; 
+  school: string;
+  sis_id: string;
+  state_id: string;
+  student_number: string;
+  // enrollments: Enrollment;
+
+  constructor(data) {
+    this.id = data.id;
+    this.name = `${data.name.first} ${data.name.middle} ${data.name.last}`;
+    this.district = data.district;
+    this.email = data.email;
+
+    // Student-only fields
+    this.dob = data.roles.student.dob;
+    this.gender = data.roles.student.gender;
+    this.grade = data.roles.student.grade;
+    this.school = data.roles.student.school;
+    this.sis_id = data.roles.student.sis_id;
+    this.state_id = data.roles.student.state_id;
+    this.student_number = data.roles.student.student_number;
+  }
+};
+
+export class Section {
+  id: string;
+  district: string;
+  grade: string;
+  name: string;
+  period: string;
+  school: string;
+  section_number: string;
+  sis_id: string;
+  students: string[];
+  subject: string; 
+  teacher: string;
+  teachers: string[];
+
+  constructor(data) {
+    this.id = data.id;
+    this.district = data.district;
+    this.grade = data.grade;
+    this.name = data.name;
+    this.period = data.period;
+    this.school = data.school;
+    this.section_number = data.section_number;
+    this.sis_id = data.sis_id;
+    this.students = data.students;
+    this.subject = data.subject; 
+    this.teacher = data.teacher;
+    this.teachers = data.teachers;
+  }
+}
+
+export type Assignment = {
+  id: string;
+}
+
+export type Submission = {
+  id: string;
+}
